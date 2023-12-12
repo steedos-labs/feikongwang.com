f628d37e037d2b73d8c5d1eafb537ff5380aaa0d
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-12-12 16:15:15 +0800

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -72,7 +72,7 @@ function Plan({
         featured ? 'order-first bg-blue-600 py-8 lg:order-none' : 'lg:py-8',
       )}
     >
-      <h3 className="mt-5 font-display text-lg text-white">{name}</h3>
+      <h3 className="mt-5 font-display text-2xl text-white">{name}</h3>
       <p
         className={clsx(
           'mt-2 text-base',
@@ -81,9 +81,10 @@ function Plan({
       >
         {description}
       </p>
-      <p className="order-first font-display text-5xl font-light tracking-tight text-white">
+      <div className="pt-4 font-display text-5xl font-light tracking-tight text-white">
         {price}
-      </p>
+        <p className="pt-2 font-normal text-lg">每10用户/年</p>
+      </div>
       <ul
         role="list"
         className={clsx(
@@ -105,7 +106,7 @@ function Plan({
         className="mt-8"
         aria-label={`Get started with the ${name} plan for ${price}`}
       >
-        Get started
+        免费试用
       </Button>
     </section>
   )
@@ -123,54 +124,52 @@ export function Pricing() {
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
             <span className="relative whitespace-nowrap">
               <SwirlyDoodle className="absolute left-0 top-1/2 h-[1em] w-full fill-blue-400" />
-              <span className="relative">版本与报价</span>
+              <span className="relative">选择适合您的版本</span>
             </span>{' '}
           </h2>
-          <p className="mt-4 text-lg text-slate-400">
+          {/* <p className="mt-4 text-lg text-slate-400">
           释放增长潜力，使用完全可定制的费控自动化软件。
-          </p>
+          </p> */}
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
             name="入门版"
-            price="$9"
-            description="Good for anyone who is self-employed and just getting started."
+            price="¥800"
+            description="线上处理电子发票，适合个人或小团队使用。"
             href="/register"
             features={[
-              'Send 10 quotes and invoices',
-              'Connect up to 2 bank accounts',
-              'Track up to 15 expenses per month',
-              'Manual payroll support',
-              'Export up to 3 reports',
+              '智能发票扫描识别',
+              '发票查重与验真',
+              '费用报销申请与审批',
+              '查看费用报告和分析',
+              '移动应用',
             ]}
           />
           <Plan
             featured
             name="专业版"
-            price="$15"
-            description="Perfect for small / medium sized businesses."
+            price="¥2000"
+            description="中小企业最佳费控管理云服务。"
             href="/register"
             features={[
-              'Send 25 quotes and invoices',
-              'Connect up to 5 bank accounts',
-              'Track up to 50 expenses per month',
-              'Automated payroll support',
-              'Export up to 12 reports',
-              'Bulk reconcile transactions',
-              'Track in multiple currencies',
+              '管理合同与对公付款',
+              '数据审计日志与安全日志',
+              '自定义流程审批单样式',
+              '自定义单据打印样式',
+              '数据导入与导出',
             ]}
           />
           <Plan
             name="企业版"
-            price="$39"
-            description="For even the biggest enterprise companies."
+            price="¥5000"
+            description="更灵活的自定义和应用集成，支持私有部署。"
             href="/register"
             features={[
-              'Send unlimited quotes and invoices',
-              'Connect up to 15 bank accounts',
-              'Track up to 200 expenses per month',
-              'Automated payroll support',
-              'Export up to 25 reports, including TPS',
+              '自定义业务字段',
+              '自定义数据分析',
+              '自定义数据访问权限',
+              '与现有业务系统集成',
+              '基于低代码平台，实现深度业务自定义',
             ]}
           />
         </div>
