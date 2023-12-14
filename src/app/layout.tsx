36120a5e5cdabfcc79c8baee17fdae0a20c7682c
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-12-14 19:12:31 +0800

    add Zoho SalesIQ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
           src="https://unpkg.steedos.cn/keycloak-js@21.0.2/dist/keycloak.js"
           defer
         ></script>
+        <script type="text/javascript" id="zsiqchat">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "siq06a2aa9876671422010a01628b2c73509eeee50b63a8c87af75f3824d231afac", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zohopublic.com.cn/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
       </head>
       <body className="flex h-full flex-col min-h-full bg-white antialiased dark:bg-zinc-900">
           {children}
