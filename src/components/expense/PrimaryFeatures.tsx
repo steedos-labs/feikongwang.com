61abe0ee6f224daf6136364a09c6d22090abed62
李征 <lizheng@hotoa.com>
2023-12-14 13:23:45 +0800

    费控首页图片调整

--- src/components/expense/PrimaryFeatures.tsx
+++ src/components/expense/PrimaryFeatures.tsx
@@ -8,7 +8,7 @@ import clsx from 'clsx'
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-features.jpg'
 import screenshotExpenses from '@/images/screenshots/expenses.png'
-import screenshotPayroll from '@/images/screenshots/payroll.png'
+import screenshotPayroll from '@/images/screenshots/payroll.gif'
 import screenshotReporting from '@/images/screenshots/reporting.png'
 import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
 
