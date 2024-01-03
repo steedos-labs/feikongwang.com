7c20f0eaa998040d04f1328d2984d172991d6405
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-01-03 18:59:30 +0800

    beta logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,7 @@
 export function Logo(props: React.ComponentPropsWithoutRef<any>) {
   return (
     <div className="group flex flex-row justify-center items-center">
-      <img className={`${props.className}`} src="/logo.svg"/>
-      <div className="ml-2 text-lg font-bold text-slate-900">费控王</div>
+      <img className={`${props.className}`} src="/logo-beta.svg"/>
     </div>
   )
 }
